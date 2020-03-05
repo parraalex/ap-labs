068bc3e61fdc8dc17b8096a53592fee59559d115
Alex Parra <alexparra_07@hotmail.com>
2020-03-05 10:17:10 -0600

    Word reverser solved
    Signed-off-by: Alex Parra <alexparra_07@hotmail.com>

--- labs/word-reverser/reverser.c
+++ labs/word-reverser/reverser.c
@@ -1,6 +1,29 @@
+
 #include <stdio.h>
 
+void reverse(int lenght, char arr[]) {
+
+    int i;
+    char tmp;
+
+    for (i = 0;  i < lenght/2; i++) {
+        tmp = arr[i];
+        arr[i] = arr[lenght - i - 1];
+        arr[lenght - i - 1] = tmp;
+    }
+}
+
+
 int main(){
-    // Place your magic here
-    return 0;
+int i = 0;
+char name[20]; 
+
+printf("\nEnter the Name : "); 
+
+while((name[i] = getchar())!='\n')
+        i++ ;
+
+reverse(i,name);
+printf("%s",name);
+return 0;
 }
