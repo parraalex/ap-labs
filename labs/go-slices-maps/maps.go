368cdaf67be313b6e8922afdade842d7055e3d9c
Alex Parra <alexparra_07@hotmail.com>
2020-03-08 12:44:11 -0600

    solve go-slices-maps AlejandroParra
    Signed-off-by: Alex Parra <alexparra_07@hotmail.com>

--- labs/go-slices-maps/maps.go
+++ labs/go-slices-maps/maps.go
@@ -1,4 +1,28 @@
 package main
 
+import (
+	"golang.org/x/tour/wc"
+	"strings"
+)
+
+func WordCount(s string) map[string]int {
+	mapa := make(map[string]int)
+	words := strings.Fields(s)
+	for i := 0; i < len(words); i++ {
+		if elem, ok := mapa[words[i]]; !ok {
+			mapa[words[i]] = 1
+			_ = elem
+		} else {
+			mapa[words[i]] = mapa[words[i]] + 1
+			_ = elem
+		}
+		
+
+
+	}
+	return mapa
+}
+
 func main() {
+	wc.Test(WordCount)
 }
--- labs/go-slices-maps/slices.go
+++ labs/go-slices-maps/slices.go
@@ -1,4 +1,24 @@
 package main
 
-func main() {
+import "golang.org/x/tour/pic"
+
+func Pic(dx, dy int) [][]uint8 {
+	a := make([][]uint8, dy)
+	for i:=0; i<dy; i++ {
+		tmp := make([]uint8, dx)
+		a[i] = tmp
+	}
+	for i:=0; i<dy; i++{
+		for j:=0; j<dx; j++{
+			a[i][j] = uint8(i^j)
+		}
+	}
+	return a
+	
 }
+
+func main() {
+	pic.Show(Pic)
+}
+
+
