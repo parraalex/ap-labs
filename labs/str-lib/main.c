5a51c874b418f38b0bbf85cb1dce44f59683869a
Alex Parra <alexparra_07@hotmail.com>
2020-03-24 13:52:00 -0600

    Solve str-lib
    Signed-off-by: Alex Parra <alexparra_07@hotmail.com>

--- labs/str-lib/main.c
+++ labs/str-lib/main.c
@@ -1,5 +1,22 @@
+//main.c
+#include <stdlib.h>
 #include <stdio.h>
 
-int main() {
+int mystrlen(char*);
+char * mystradd(char*,char*);
+int mystrfind(char*,char*);
+
+
+int main(int argc, char **argv){
+    printf("Initial Lenght      : %d\n",mystrlen(argv[1]));
+    char *newStr = mystradd(argv[1], argv[2]);
+    printf("New String          : %s\n",newStr);
+    int result = mystrfind(newStr, argv[3]);
+    char *yesno = "yes";
+    if(result == 0){
+        yesno = "no";
+    }
+    printf("SubString was found : %s\n",yesno);
     return 0;
 }
+
--- labs/str-lib/strlib.c
+++ labs/str-lib/strlib.c
@@ -1,13 +1,39 @@
+//strlib.c
+#include <stdlib.h>
 
 
-int mystrlen(char *str){
-    return 0;
+int mystrlen(char *str) {
+    int len = 0;
+    for(int i = 0; str[i] != 0; i++){
+        len++;
+    }
+    return len;
 }
 
-char *mystradd(char *origin, char *addition){
-    return 0;
+char * mystradd(char *first, char *last) {
+    int len = mystrlen(first) + mystrlen(last);
+    char *final = malloc(sizeof(char)*len);
+    for(int i = 0; i < mystrlen(first); i++){
+        final[i] = first[i];
+    }
+    int count = 0;
+    for(int i = mystrlen(first); i < len; i++){
+        final[i] = last[count];
+        count++;
+    }
+    return final;
 }
 
-int mystrfind(char *origin, char *substr){
+int mystrfind(char *complete, char *substr){
+    int lenComplete = mystrlen(complete);
+    int lenSub = mystrlen(substr);
+
+    for (int i = 0; i < lenComplete-lenSub+1; i++){
+        for(int j = 0; j < lenSub; j++) {
+            if(substr[j] == complete[i+j]){
+                return 1;
+            }
+        }
+    }
     return 0;
 }
