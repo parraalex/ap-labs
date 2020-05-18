069f66138be299b1bb783b8c8b208a8a525191fc
Alex Parra <alexparra_07@hotmail.com>
2020-05-18 12:20:56 -0500

    solve web-crawler
    Signed-off-by: Alex Parra <alexparra_07@hotmail.com>

--- labs/web-crawler/crawl3.go
+++ labs/web-crawler/crawl3.go
@@ -1,21 +1,11 @@
-// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
-// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
-
-// See page 241.
-
-// Crawl2 crawls web links starting with the command-line arguments.
-//
-// This version uses a buffered channel as a counting semaphore
-// to limit the number of concurrent calls to links.Extract.
-//
-// Crawl3 adds support for depth limiting.
-//
 package main
 
 import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
+	"strings"
 
 	"gopl.io/ch5/links"
 )
@@ -24,6 +14,7 @@ import (
 // tokens is a counting semaphore used to
 // enforce a limit of 20 concurrent requests.
 var tokens = make(chan struct{}, 20)
+var depth = 0
 
 func crawl(url string) []string {
 	fmt.Println(url)
@@ -41,27 +32,60 @@ func crawl(url string) []string {
 
 //!+
 func main() {
+
+	if len(os.Args) < 3 {
+		println("error, please specify parameters: ./crawl <-depth=x> <url>")
+		os.Exit(1)
+	}
+	lista := []string{}
+	d := strings.Split(os.Args[1], "=")
+	if d[0] != os.Args[1] {
+		dtmp, flag := strconv.Atoi(d[1])
+		if d[0] == "-depth" && flag == nil {
+			depth = dtmp
+			lista = append(lista, d[1])
+		} else {
+			println("error. please use correct parameters: ./crawl <-depth=x> <url")
+			os.Exit(2)
+		}
+	} else {
+		println("error. please use correct parameters: ./crawl <-depth=x> <url")
+		os.Exit(3)
+	}
+	lista = append(lista, os.Args[2:]...)
 	worklist := make(chan []string)
 	var n int // number of pending sends to worklist
 
 	// Start with the command-line arguments.
 	n++
+
 	go func() { worklist <- os.Args[1:] }()
 
 	// Crawl the web concurrently.
 	seen := make(map[string]bool)
 	for ; n > 0; n-- {
 		list := <-worklist
-		for _, link := range list {
+		depthOnWorklist := 0
+		for num, link := range list {
+			if num == 0 {
+				depthOnWorklist, _ = strconv.Atoi(link)
+				if depthOnWorklist <= depth {
+					continue
+				} else {
+					break
+				}
+			}
 			if !seen[link] {
 				seen[link] = true
 				n++
 				go func(link string) {
-					worklist <- crawl(link)
+					linkurl := crawl(link)
+					linkurl = append([]string{strconv.Itoa(depthOnWorklist + 1)}, linkurl...)
+					worklist <- linkurl
 				}(link)
 			}
 		}
 	}
+	println("done")
 }
 
-//!-
